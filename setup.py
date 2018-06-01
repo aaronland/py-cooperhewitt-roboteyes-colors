5623df62a087819115a5cf2b9923ae65d9862d68
thisisaaronland <straup@gmail.com>
2018-06-01 16:34:40 -0700

    pi wrangling; bump to 0.2.0

--- setup.py
+++ setup.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python
 
+import os
+import sys
+
+setup = os.path.abspath(sys.argv[0])
+parent = os.path.dirname(setup)
+pkg = os.path.basename(parent)
+
+if pkg.startswith("py-cooperhewitt"):
+    pkg = pkg.replace("py-", "")
+    pkg = pkg.replace("-", ".")
+
+    egg_info = "%s.egg-info" % pkg
+    egg_info = os.path.join(parent, egg_info)
+
+    if os.path.exists(egg_info):
+        shutil.rmtree(egg_info)
+
 from setuptools import setup, find_packages
 
 packages = find_packages()
-desc = open("README.md").read(),
+desc = open("README.md").read()
+version = open("VERSION").read()
 
 setup(
     name='cooperhewitt.roboteyes.colors',
     namespace_packages=['cooperhewitt', 'cooperhewitt.roboteyes'],
-    version='0.1',
+    version=version,
     description='',
     author='Cooper Hewitt Smithsonian Design Museum',
     url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-colors',
-    install_requires=['roygbiv'],
     packages=packages,
-    scripts=[],
-    download_url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-colors/releases/tag/v0.1',
+    download_url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-colors/releases/tag/' + version,
     license='BSD')
